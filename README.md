4d3c34da32f7452e7fba11e778516d2c6c705bb4
趙惟倫 <bluebat@member.fsf.org>
2017-08-14 18:22:40 +0800

    new version

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ CNS11643-Data
 =============
 
 Derived from CNS11643
-* Original Page: http://data.gov.tw/node/5961
-* License Page: http://data.gov.tw/?q=principle
-* Original File: http://www.cns11643.gov.tw/AIDB/Open_Data.zip
-* Original Date: 2014-01-23
-
+* Original Page: https://data.gov.tw/dataset/5961
+* Original License: Open Government Data License, version 1.0
+  (compatible with the Creative Commons Attribution 4.0 International)
+* License Page: https://data.gov.tw/license#eng
+* Original File: https://www.cns11643.gov.tw/AIDB/Open_Data.zip
+* Original Date: 2017-06-05
---
+++ collation/mk-cns11643_stroke.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+echo '% This file is part of the GNU C Library and contains locale data.'
+echo '% The Free Software Foundation does not claim any copyright interest'
+echo '% in the locale data contained in this file.  The foregoing does not'
+echo '% affect the license of the GNU C Library as a whole.  It does not'
+echo '% exempt you from the conditions of the license if your use would'
+echo '% otherwise be governed by that license.'
+echo
+echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+echo '%'
+echo '% Collation for Hanzi(chinese characters) by component and stroke'
+echo '%'
+echo '% Original data from CNS11643 at http://www.cns11643.gov.tw/'
+echo '% License: Open Government Data License 1.0 (compatible with the CC BY 4.0)'
+echo '%'
+echo '% Prepared and contributed to glibc by Wei-Lun Chao <bluebat@member.fsf.org>'
+echo '% Project at https://github.com/chinese-opendesktop/cns11643-data'
+echo '%'
+echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+echo
+echo 'LC_COLLATE'
+echo
+echo 'copy "iso14651_t1_common"'
+echo
+echo 'script <HAN>'
+echo
+echo 'order_start <HAN>;forward;forward;forward;forward,position'
+
+sort -n -k 2 uni_component_stroke_sequence.txt |
+sed -e 's/^0\(.*\) /<U\1> /' -e 's/^2\(.*\) /<U0002\1> /' |
+sed 's/ /;IGNORE;IGNORE;IGNORE # /' |
+sed 's/\(<.*>\)/\1 \1/'
+
+echo '#'
+echo 'order_end'
+echo '#'
+echo 'END LC_COLLATE'
